8c857ba1058077e828e29f6e7b927e6a81094a92
龙泽桢 <long_zezhen@sina.com>
2019-04-19 11:44:33 +0800

    Initial Commit

---
+++ LZZFilmPutinto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZZFilmPutinto
+//
+//  Created by 龙泽桢 on 2019/4/19.
+//  Copyright © 2019 tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZZFilmPutintoTests/LZZFilmPutintoTests.m
@@ -0,0 +1,37 @@
+//
+//  LZZFilmPutintoTests.m
+//  LZZFilmPutintoTests
+//
+//  Created by 龙泽桢 on 2019/4/19.
+//  Copyright © 2019 tools. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LZZFilmPutintoTests : XCTestCase
+
+@end
+
+@implementation LZZFilmPutintoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
